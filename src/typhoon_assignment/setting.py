e8a95aca6821b719221a63169f82ca48adb24db7
TanukiDong <eurondelta@gmail.com>
2024-09-13 20:37:56 +0700

    setting

--- src/typhoon_assignment/setting.py
+++ src/typhoon_assignment/setting.py
@@ -5,5 +5,7 @@ load_dotenv(".envrc")
 # load_dotenv(".env")
 
 API_KEY = os.getenv("API_KEY", "")
-MODEL = "typhoon-instruct"
+MODEL = "typhoon-v1.5x-70b-instruct"
 BASE_URL= "https://api.opentyphoon.ai/v1"
+# REPETITION_PENALTY = 1.05
+MAX_TOKENS = 500
